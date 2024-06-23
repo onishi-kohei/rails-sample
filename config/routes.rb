cbf2fa35d8b0deefbd62aaba346bf8b996721cd6
onishi.kohei <kohei.o2222@gmail.com>
2024-06-23 15:10:56 +0900

    users/newの新規作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get 'users', to: 'users#index'
+  get '/users', to: 'users#index'
+  get '/users/new'
 end
